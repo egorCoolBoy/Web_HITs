f2e88b4cd3a28acf368ae8ac3047c4da5162138a
egorCoolBoy <ega02022007@gmail.com>
2025-04-04 01:57:55 +0700

    nothing

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById('Ant').addEventListener('click', function() {
     window.location.href = "./Ant Alg/index.html";
 });
+document.getElementById('A*').addEventListener('click', function() {
+    window.location.href = "./Astar/index.html";
+});
