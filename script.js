b2a826dd4001ff6b4dabed02c8a89cc7036af27e
egorCoolBoy <ega02022007@gmail.com>
2025-04-04 03:00:04 +0700

    added files

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 document.getElementById('Ant').addEventListener('click', function() {
-    window.location.href = "./Ant Alg/index.html";
+    window.location.href = "./Ant Alg/Ant Alg.html";
 });
 document.getElementById('A*').addEventListener('click', function() {
-    window.location.href = "./Astar/index.html";
+    window.location.href = "./Astar/Astar.html";
+});
+document.getElementById('Gen Alg').addEventListener('click', function() {
+    window.location.href = "./Gen Alg/Gen Alg.html";
+});
+document.getElementById('Neural Net').addEventListener('click', function() {
+    window.location.href = "./Neural Net/NN.html";
+});
+document.getElementById('Clust Alg').addEventListener('click', function() {
+    window.location.href = "./Clust Alg/Clust Alg.html";
+});
+document.getElementById('Dec Tree').addEventListener('click', function() {
+    window.location.href = "./Dec Tree/Dec Tree.html";
 });
