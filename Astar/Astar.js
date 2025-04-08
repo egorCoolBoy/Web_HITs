db844c2b8295186e72321a30b293e0ae7dba275b
egorCoolBoy <ega02022007@gmail.com>
2025-04-09 03:10:22 +0700

    added createMAp

--- Astar/Astar.js
+++ Astar/Astar.js
@@ -1,8 +1,25 @@
-function showInputValue() {
-        const inputElement = document.getElementById('myInput');
-        const number = inputElement.value; 
-        const result = number  
-        const outputElement = document.getElementById('output');
-        outputElement.innerText = result;
-        
+function createMap() {
+        const size = parseInt(document.getElementById('size').value);
+        const grid = document.getElementById('grid');
+        grid.innerHTML = '';
+        grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    
+        for (let i = 0; i < size * size; i++) 
+        {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.addEventListener('click', () => { cell.classList.toggle('startEnd'); 
+            });
+            cell.addEventListener('contextmenu', (e) => { 
+                e.preventDefault(); 
+                cell.classList.toggle('wall'); 
+            });
+            grid.appendChild(cell);
+        }
 }
+
+function findWay()
+{
+        document.writeln("СКОРО")
+}
+    
