997d400ed34107ec3391fb371d0e5b9389608478
egorCoolBoy <ega02022007@gmail.com>
2025-04-08 13:22:50 +0700

    start astar

---
+++ Astar/Astar.js
@@ -0,0 +1,8 @@
+function showInputValue() {
+        const inputElement = document.getElementById('myInput');
+        const number = inputElement.value; 
+        const result = number  
+        const outputElement = document.getElementById('output');
+        outputElement.innerText = result;
+        
+}
--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-document.getElementById('Ant').addEventListener('click', function() {
+document.getElementById('Ant').addEventListener('click', function()
+ {
     window.location.href = "./Ant Alg/Ant Alg.html";
 });
-document.getElementById('A*').addEventListener('click', function() {
+document.getElementById('A*').addEventListener('click', function() 
+{
     window.location.href = "./Astar/Astar.html";
 });
-document.getElementById('Gen Alg').addEventListener('click', function() {
+document.getElementById('Gen Alg').addEventListener('click', function()
+ {
     window.location.href = "./Gen Alg/Gen Alg.html";
 });
-document.getElementById('Neural Net').addEventListener('click', function() {
+document.getElementById('Neural Net').addEventListener('click', function() 
+{
     window.location.href = "./Neural Net/NN.html";
 });
-document.getElementById('Clust Alg').addEventListener('click', function() {
+document.getElementById('Clust Alg').addEventListener('click', function() 
+{
     window.location.href = "./Clust Alg/Clust Alg.html";
 });
-document.getElementById('Dec Tree').addEventListener('click', function() {
+document.getElementById('Dec Tree').addEventListener('click', function() 
+{
     window.location.href = "./Dec Tree/Dec Tree.html";
 });
